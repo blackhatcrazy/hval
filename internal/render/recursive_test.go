696880c97e9bb8cf766b6d09339eb43dcce44f67
JanFokken <jan.fokken@sap.com>
2019-10-28 22:40:20 +0100

    wip

--- internal/render/recursive_test.go
+++ internal/render/recursive_test.go
@@ -2,7 +2,10 @@ package render
 
 import (
 	"fmt"
+	"log"
 	"testing"
+
+	"gopkg.in/yaml.v2"
 )
 
 var testTmpl = `
@@ -11,28 +14,40 @@ t1: {{ printf "hello-%s" .k2 }}
 t2: {{ .t1 }}{{ .t1 }}
 t3: {{ .t2 }} bla
 v4:
-	v41: test
+  v41: test
 t4: {{ .v4 }}
 t5:
   t51: {{ .t4 }}
 normal: value
 `
 
+type myMap map[string]interface{}
+
+func (m myMap) String() string {
+	res := "{"
+	for k, v := range m {
+		res = fmt.Sprintf("%s%s: %s, ", res, k, v)
+	}
+	return fmt.Sprintf("%s}", res[:len(res)-2])
+}
+
 var testInput = map[string]interface{}{
 	"k2":     "world",
 	"t1":     `{{ printf "hello-%s" .k2 }}`,
 	"t2":     `{{ .t1 }}{{ .t1 }}`,
 	"t3":     `{{ .t2 }} bla`,
-	"v4":     map[string]interface{}{"v41": "test"},
+	"v4":     myMap{"v41": "test"},
 	"t4":     `{{ .v4 }}`,
-	"t5":     map[string]interface{}{"t51": `{{ .t4 }}`},
+	"t5":     myMap{"t51": `{{ .t4 }}`},
 	"normal": "value",
 }
 
 func TestRender(t *testing.T) {
 	templ := tmpl{}
 	templ.dataCurrent = []byte(testTmpl)
-	templ.input = testInput
+	newMap := myMap(testInput)
+	templ.input = newMap
+	fmt.Println(newMap)
 	for templ.hasChanged() {
 		templ.render()
 		fmt.Println("it ", templ.iteration)
@@ -40,6 +55,13 @@ func TestRender(t *testing.T) {
 		fmt.Printf("err %v\n", templ.err)
 		fmt.Println("data", string(templ.dataCurrent))
 	}
+	var tr map[string]interface{}
+
+	err := yaml.Unmarshal(templ.dataCurrent, &tr)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- t:\n%v\n\n", tr)
 
 	t.FailNow()
 
